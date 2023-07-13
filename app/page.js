64df4457a5358b7684dda34ffe685dc2b437f8a1
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2023-07-13 22:37:15 +0700

    calculator app

--- app/page.js
+++ app/page.js
@@ -1,95 +1,186 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import styles from "./page.module.css"
+import { useState } from "react"
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [cache, setCache] = useState()
+    const [current, setCurrent] = useState("0")
+    const [operator, setOperator] = useState()
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const round = (num) => {
+        return +parseFloat(num).toFixed(6)
+    }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    const handleNum = (num) => {
+        if (current.length == 6) {
+            return
+        }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        if (current == "0") {
+            setCurrent(num)
+        } else {
+            setCurrent(current + num)
+        }
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+    const handleClear = () => {
+        setCurrent("0")
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    const flipSign = () => {
+        setCurrent(-parseFloat(current))
+    }
+
+    const percentage = () => {
+        setCurrent(round(current / 100))
+    }
+
+    const operate = (op) => {
+        if (cache) {
+            resolve()
+        }
+        setCache(current)
+        setCurrent("0")
+        setOperator(op)
+    }
+
+    const resolve = () => {
+        if (!operator) {
+            return
+        }
+
+        if (operator === "/") {
+            setCurrent(cache / current)
+        }
+    }
+
+    return (
+        <main className={styles.main}>
+            <div className={styles.wrapper}>
+                <div className={styles.screen}>{current}</div>
+                <div className={styles.row}>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleClear()}
+                    >
+                        C
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => flipSign()}
+                    >
+                        +/-
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => percentage()}
+                    >
+                        %
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => operate("/")}
+                    >
+                        ÷
+                    </button>
+                </div>
+                <div className={styles.row}>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("7")}
+                    >
+                        7
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("8")}
+                    >
+                        8
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("9")}
+                    >
+                        9
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => operate("x")}
+                    >
+                        ×
+                    </button>
+                </div>
+                <div className={styles.row}>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("4")}
+                    >
+                        4
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("5")}
+                    >
+                        5
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("6")}
+                    >
+                        6
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => operate("-")}
+                    >
+                        -
+                    </button>
+                </div>
+                <div className={styles.row}>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("1")}
+                    >
+                        1
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("2")}
+                    >
+                        2
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("3")}
+                    >
+                        3
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => operate("+")}
+                    >
+                        +
+                    </button>
+                </div>
+                <div className={styles.row}>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum("0")}
+                    >
+                        0
+                    </button>
+                    <button
+                        className={styles.button}
+                        onClick={() => handleNum(".")}
+                    >
+                        .
+                    </button>
+                    <button className={styles.button} onClick={() => resolve()}>
+                        =
+                    </button>
+                </div>
+            </div>
+        </main>
+    )
 }
